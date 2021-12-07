109ee5a4a78809c9e16c66654a607a550514c69c
Subramaniyam <ksubramani@wiley.com>
2021-12-07 22:43:14 +0530

    Update test and fix pom

--- AccessibilityTest/src/test/java/com/wiley/allytest/GoogleHomeTest.java
+++ AccessibilityTest/src/test/java/com/wiley/allytest/GoogleHomeTest.java
@@ -4,8 +4,8 @@ import com.deque.axe.AXE;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -16,35 +16,50 @@ import java.net.URL;
 
 public class GoogleHomeTest {
 
-    WebDriver driver;
     private static final URL scriptUrl = GoogleHomeTest.class.getResource("/axe.min.js");
+    WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().setup();
 
-//        WebDriver driver = new RemoteWebDriver("http://localhost:9515", DesiredCapabilities.chrome());
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.google.com");
     }
 
     @Test
-    public void googleHomeTest(){
-        JSONObject response = new AXE.Builder(driver, scriptUrl).analyze();
+    public void googleHomeTest() {
+        JSONObject response;
+        response = new AXE.Builder(driver, scriptUrl).analyze();
         JSONArray violations = response.getJSONArray("violations");
 
-        if(violations.length()==0){
+        if (violations.length() == 0) {
             System.out.println("NO ISSUES");
-        }else{
-            AXE.writeResults("googleHomeTest",response);
-            Assert.assertTrue(false,AXE.report(violations));
+        } else {
+            AXE.writeResults("googleHomeTest", response);
+//            Assert.assertTrue(false,AXE.report(violations));
+            Assert.fail(AXE.report(violations));
 
         }
 
     }
 
+    @Test
+    public void testUsedWebElement() {
+        JSONObject response;
+        response = new AXE.Builder(driver, scriptUrl).analyze(driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/img")));
+        JSONArray violations = response.getJSONArray("violations");
+
+        if (violations.length() == 0) {
+            System.out.println("No Violations Found in Web Element");
+        } else {
+            AXE.writeResults("testUsedWebElement", response);
+            Assert.fail(AXE.report(violations));
+        }
+    }
+
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
